1021dff688d987ceb359e7d0e45b782ecdd9e8a7
fire-walker <charlotte.churcheward@gmail.com>
2022-11-14 15:49:59 +0100

    Ajout du routing de l'application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import AboutUs from "./pages/AboutUs";
+import Error from "./pages/Error";
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import '../src/sass/_index.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/aboutus" element={<AboutUs />} />
+                <Route path="/error" element={<Error />} />
+            </Routes>
+            <Footer />
+        </Router>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
