e176d3b1927e0ef9044f0014b7a6173ac976906d
fire-walker <charlotte.churcheward@gmail.com>
2022-11-14 15:48:38 +0100

    Ajout fichiers scss + routing de l'application + differentes pages + composants header et footer + logos

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import logo_white from '../../assets/logo_white.png'
+
+
+function Footer () {
+    return (
+        <footer className='footer'>
+            <img src={logo_white} alt="logo" className='footer__logo'/>
+            <p className='footer__copyright'>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
+function Header () {
+    return (
+        <header className='header'>
+            <img src={logo} alt="logo" className='header__logo'/>
+            <nav>
+                <Link to="/" className='header__menu'>Accueil</Link>
+                <Link to="/aboutus" className='header__menu'>A propos</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
