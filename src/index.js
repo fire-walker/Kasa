7886205a3f326a9934e6257efdadd4192e09ce78
fire-walker <charlotte.churcheward@gmail.com>
2022-11-21 15:43:02 +0100

    Refactorisation du rooting

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
-import Error from "./pages/Error";
-import Header from './components/Header';
-import Footer from "./components/Footer";
-import '../src/sass/_index.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Error from "./components/Error";
 
+import "../src/sass/_index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/aboutus",
+        element: <AboutUs />,
+    },
+    {
+        path: "*",
+        errorElement: <Error />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/aboutus" element={<AboutUs />} />
-                <Route path="/error" element={<Error />} />
-            </Routes>
-            <Footer />
-        </Router>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
