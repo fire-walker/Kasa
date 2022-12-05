b04166b788ff681a27b7a4d626bf4ccc104a4533
fire-walker <charlotte.churcheward@gmail.com>
2022-12-05 15:51:57 +0100

    Modification du composant ratings

--- src/components/Accommodation/Ratings.jsx
+++ src/components/Accommodation/Ratings.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { HiStar } from "react-icons/hi";
 
-export default function Rating(rate) {
+export default function Rating({rate}) {
     const totalStars = 5;
-    const activeStars = rate;
 
     return (
         <div>
-            {[...new Array(totalStars)].map((stars, index) => {
-                return index < activeStars ? <HiStar className="fullstar"/> : <HiStar className="emptystar" />;
+            {[...new Array(totalStars)].map((star, index) => {
+                return index < rate ?
+                    <HiStar className="fullstar"/>
+                    :
+                    <HiStar className="emptystar" />;
             })}
         </div>
     );
